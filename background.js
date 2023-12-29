18ab0d33986ea1971c2de09ba38f2fbf40c75eb3
tamikazi <tahmidkazi829@gmail.com>
2023-12-29 15:46:42 -0700

    moved functionality to background.js, notifications not working currently

--- background.js
+++ background.js
@@ -1,70 +1,73 @@
 // background.js runs in chrome background. popup.js sends messages. popup.js sends messages to background.js to communicate.
 
 //Tracks remaining timer time 
-let remainingTime;
+let remainingTime = 5;
 // Time in milliseconds to decrement timer by
 let timerInterval;
 
-// Flags for 'Start' and 'Pause' buttons
-let started = false;
-let paused = false;
+let defaultTime = 5;
 
-// 40 Minutes - ** Change this to 3 seconds for testing**
-let defaultTime = 2400;
+// Flag for timer running and paused
+let isTimerRunning = false;
 
 // Handles messages, invokes function based on message. Checks whether 'start' and 'pause' flags are set to prevent unexpected results.
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.command === 'start') {
-    if (started === false) {
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.command === 'start' && !isTimerRunning) {
       startTimer();
-      started = true;
-      paused = false;
-    }
-  } else if(request.command == 'pause'){
-    if (paused === false) {
+      sendResponse({ status: "Timer started" });
+  } else if (request.command === 'pause') {
       pauseTimer();
-      paused = true;
-      started = false;
-    }
-  }else if (request.command === 'reset') {
-    resetTimer();
-  } else if (request.command === 'getRemainingTime') {
-    sendResponse({ remainingTime });
+      sendResponse({ status: "Timer paused" });
+  } else if (request.command === 'reset') {
+      resetTimer();
+      sendResponse({ status: "Timer reset" });
   }
+  // Sends remaining time to popup.js
+  else if (request.command === 'getRemainingTime') {
+      sendResponse({ remainingTime: remainingTime });
+  }
+  // Add a catch-all response for unhandled commands
+  else {
+      sendResponse({ status: "Command not recognized" });
+  }
+  return true; // Keep the message channel open for async response
 });
 
 // Starts timer countdown
 function startTimer() {
-  // Set to default time to initialize
-  if (remainingTime === undefined){
-    remainingTime = defaultTime;
-  } 
-  sendRemainingTime();
-  timerInterval = setInterval(function () {
-    if (remainingTime <= 0){
-        clearInterval(timerInterval);
-    }else{
-        remainingTime--;
-        sendRemainingTime();
+    if (!isTimerRunning) {
+        isTimerRunning = true;
+        remainingTime = remainingTime || defaultTime; // or your default time
+        timerInterval = setInterval(() => {
+            remainingTime--;
+            if (remainingTime <= 0) {
+                clearInterval(timerInterval);
+                isTimerRunning = false;
+                showNotification('Time is up!');
+                remainingTime = defaultTime; // Reset to default or a defined work/break period
+            }
+        }, 1000);
     }
-  }, 1000);
-
 }
 
 // Stops and clears the remaining time
 function resetTimer() {
-  started = false;
-  paused = false;
-  remainingTime = defaultTime;
+  clearInterval(timerInterval);
+  remainingTime = defaultTime; // Reset to default time
+  isTimerRunning = false;
 }
 
 // Pauses timer 
 function pauseTimer() {
     clearInterval(timerInterval);
-    sendRemainingTime();
+    isTimerRunning = false;
   }
 
-  // Set the remaining time left
-function sendRemainingTime() {
-  chrome.runtime.sendMessage({ command: 'updateTimer', remainingTime });
+function showNotification(message) {
+  chrome.notifications.create('', {
+      title: 'Pomodoro Timer',
+      message: message,
+      type: 'basic',
+      iconUrl: './images/light-bulb.png'
+  });
 }
--- popup/popup.js
+++ popup/popup.js
@@ -1,96 +1,35 @@
-// Time in milliseconds to decrement timer by
-let timerInterval;
-
-// Flags for 'Start' and 'Pause' buttons
-let started = false;
-let paused = false;
-
-// 40 Minutes - ** Change this to 3 seconds for testing **
-let defaultTime = 2400;
-
-// Listeners for Buttons. Checks whether 'start' and 'pause' flags are set to prevent unexpected results.
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.command === "start") {
-        if (started === false) {
-            startTimer();
-            started = true;
-            paused = false;
-        }
-    } else if(request.command == 'pause'){
-        if (paused === false) {
-            pauseTimer();
-            paused = true;
-            started = false;
-          }
-      }else if (request.command === 'reset') {
-        resetTimer();
-      }
-});
-
 // Link 'Start' button to html element
 document.getElementById("startTimerButton").addEventListener('click', function() {
-    startTimer();
-})
+    chrome.runtime.sendMessage({command: 'start'});
+});
 
-// Link 'Start' button to html element
+// Link 'Pause' button to html element
 document.getElementById("pauseTimerButton").addEventListener('click', function() {
-    pauseTimer();
-})
+    chrome.runtime.sendMessage({command: 'pause'});
+});
 
 // Link 'Reset' button to html element
 document.getElementById("resetTimerButton").addEventListener('click', function() {
-    resetTimer();
-})
-
-
-// Starts timer countdown
-function startTimer() {
-    chrome.runtime.sendMessage({command:'start'});
-    updateTimer();
-
-    // Decrements by 1 second
-    timerInterval  = setInterval(updateTimer,1000);
-
-}
-// Pauses timer, preserving time 
-function pauseTimer() {
-    chrome.runtime.sendMessage({command:'pause'});
-    clearInterval(timerInterval);
-    updateTimer();
-}
-
-// Resets timer to default
-function resetTimer() {
-    chrome.runtime.sendMessage({command:'reset'});
-    // Resets 'Start' and 'Pause' button flags 
-    started = false;
-    paused = false;
-
-    // Resets remaining time 
-    remainingTime = defaultTime;
-    updateTimer();
-    pauseTimer();
-}
+    chrome.runtime.sendMessage({command: 'reset'});
+});
 
 // Updates timer - formats remaining time to be displayed 
-function updateTimer(){
-    chrome.runtime.sendMessage({command:'getRemainingTime'},(response) =>{
-        if (response && response.remainingTime){
-            const minutes = Math.floor(response.remainingTime /60);
+function updateDisplay() {
+    chrome.runtime.sendMessage({ command: 'getRemainingTime' }, function(response) {
+        if (response && response.remainingTime) {
+            const minutes = Math.floor(response.remainingTime / 60);
             const seconds = response.remainingTime % 60;
-            document.getElementById('time').textContent=`${padZero(minutes)}:${padZero(seconds)}`; 
+            document.getElementById('timerDisplay').textContent = `${padZero(minutes)}:${padZero(seconds)}`;
         }
-        // If timer has reached zero, displays zero.
-        else{
-            document.getElementById('time').textContent=`00:00`;
-        }
-    })
+    });
 }
 
 // Pads with zero when timer <10 minutes
 function padZero(num){
     return num < 10 ?`0${num}` : num;
 }
+
+setInterval(updateDisplay, 1000);
 
 // Opens tab to extenion's page
 document.getElementById("openMain").addEventListener('click', function() {
